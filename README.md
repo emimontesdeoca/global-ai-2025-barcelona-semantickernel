c8027a46b2fa3fdae72bd3c817c97ecc22e9c489
Emiliano Montesdeoca <emiliano.montesdeoca@intelequia.com>
2025-03-04 11:12:16 +0000

    feat: initial files

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# global-ai-2025-barcelona-semantickernel
+# global-ai-2025-madrid-semantickernel
---
+++ src/SemanticKernelSession.Tasks/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Hello World!");
+
+// Add OPENAI_ENDPOINT
+// Add OPENAI_APIKEY
+// DeploymentName gpt-4o-mini
+// AzureOpenAIChatCompletionService
+// Basic demo
+// Loop demo
+// History
+// Multimodal ImageContent + ChatMessageContentItemCollection
+// Add Builder + Kernel.CreateBuilder() + builder.Build()
+// Add Kernel + AzureOpenAIPromptExecutionSettings
+// Add plugin + debug + history
+// Use planner + TaskPlugin
+// Update persona
